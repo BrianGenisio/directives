f9c30557f44c788c9c4f4e38def18e056f42aba7
Brian Genisio <BrianGenisio@Gmail.com>
2013-11-10 21:47:57 -0500

    concatinating files

--- gruntfile.js
+++ gruntfile.js
@@ -2,13 +2,20 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    concat: {
+        options: { separator: ';' },
+        dist: {
+            src: ['js/**/*.js'],
+            dest: 'dist/app.js'
+        }
+    },
     jshint: {
         all: ['js/*.js']
     },
     watch: {
         scripts: {
             files: '**/*.js',
-            tasks: ['jshint'],
+            tasks: ['jshint', 'concat'],
             options: {
                 livereload: false
             }
@@ -23,10 +30,11 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-livereload');
     
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'concat']);
 
 };
