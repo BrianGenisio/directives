b67d5f3e0e2deac2f63d350fc618f3f0bf13a07a
Brian Genisio <BrianGenisio@Gmail.com>
2013-11-16 22:06:27 -0500

    supporting saving of files via the browser.

--- app/directives/show-code.js
+++ app/directives/show-code.js
@@ -6,6 +6,13 @@ App.directive('showCode', function($http, $timeout) {
 		},
 		link: function(scope, element, attrs) {
 
+			scope.save = function() {
+				$http.post("/save", {
+					file: scope.url,
+					content: scope.code
+				});
+			};
+
 			scope.$watch('url', function() {
 				var mode = scope.mode ? 'text/' + scope.mode : 'text/html';
 
@@ -23,8 +30,9 @@ App.directive('showCode', function($http, $timeout) {
 					.success(function(code) {
 						scope.code = code;
 					});
-			})
+			});
 		},
-		template: '<textarea ng-if="code" ui-codemirror="editorOptions" ng-model="code"></textarea>'
+		template: '<textarea ng-if="code" ui-codemirror="editorOptions" ng-model="$parent.code"></textarea>' +
+		          '<button ng-click="save()" class="btn btn-default">Save</button>'
 	};
 });
--- server.js
+++ server.js
@@ -1,6 +1,37 @@
-var connect = require('connect');
- 
-var app = connect()
-    .use(connect.static(__dirname))
-    .listen(3000);
+//var connect = require('connect');
+// 
+//var app = connect()
+//    .use(connect.static(__dirname))
+//    .listen(3000);
+
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+app.use(express.bodyParser());
+
+app.get("/", function(request, response) {
+	response.sendfile(__dirname + "/index.html");
+});
+
+app.get("/*", function(request, response) {
+	response.sendfile(__dirname + request.url);
+});
+
+app.post("/save", function(request, response) {
+	var data = request.body;
+	var fileName = __dirname + data.file;
+
+	fs.writeFile(fileName, data.content, function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("Saved File: ", fileName);
+		}
+	});
+
+	response.send("");
+});
+
+app.listen(3000);
 
