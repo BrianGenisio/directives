fcf466ac919067925fa09e006d9fff50f9f2b045
Brian Genisio <BrianGenisio@Gmail.com>
2013-11-17 22:10:45 -0500

    Showing an example of animating ng-view

--- app/module.js
+++ app/module.js
@@ -1,2 +1,2 @@
 angular.module('templates', []);
-window.App = angular.module('SlidePlayer', ['ngRoute', 'ui.codemirror', 'templates']);
+window.App = angular.module('SlidePlayer', ['ngRoute', 'ngAnimate', 'ui.codemirror', 'templates']);
