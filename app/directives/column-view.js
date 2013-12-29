f96f49e1f5385c4755641b5c45a0364c12936c8d
Brian Genisio <BrianGenisio@Gmail.com>
2013-12-28 20:11:28 -0500

    creating a column view to animate between content easier

---
+++ app/directives/column-view.js
@@ -0,0 +1,53 @@
+App.directive('columnView', function() {
+  return {
+    restrict: 'AE',
+    transclude: true,
+    scope: {
+      heading: '@'
+    },
+    controller: function($scope) {
+      var columns = [];
+
+      this.register = function(column) {
+        columns.push(column);
+        this.focus(columns[0]);
+      };
+
+      this.focus = function(focusedColumn) {
+        if(!focusedColumn) return;
+
+        var numberOfColumns = columns.length;
+        angular.forEach(columns, function(column, i) {
+          column.width = 2;
+        });
+
+        focusedColumn.width = 13 - (numberOfColumns * 2);
+      };
+    },
+    templateUrl: '/app/directives/column-view.html'
+  };
+});
+
+App.directive('column', function() {
+  return {
+    restrict: 'AE',
+    transclude: true,
+    require: '^columnView',
+    scope: {},
+    controller: function($scope) {
+      $scope.getColumn = function() {
+        return 'col-md-' + $scope.width;
+      };
+    },
+    link: function(scope, element, attrs, viewCtrl) {
+      viewCtrl.register(scope);
+
+      element.on('click', function() {
+        scope.$apply(function() {
+          viewCtrl.focus(scope);
+        });
+      });
+    },
+    template: '<div class="column" ng-class="getColumn()" ng-transclude></div>'
+  }
+});
--- app/module.js
+++ app/module.js
@@ -1,2 +1,6 @@
 angular.module('templates', []);
 window.App = angular.module('SlidePlayer', ['ngRoute', 'ngAnimate', 'ui.codemirror', 'templates']);
+
+window.App.config(function($animateProvider){
+  $animateProvider.classNameFilter(/^((?!col-md).)*$/);
+});
